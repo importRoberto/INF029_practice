e3fb79d81bb63982d7336df8fe05ebbafa0f1c25
Roberto Barreto <robertoboficial@gmail.com>
2024-11-25 12:29:34 -0300

    1st function complete

--- 1st_function.c
+++ 1st_function.c
@@ -3,3 +3,23 @@
 valores, enviar para a função soma. A função soma deve realizar a soma e retornar o valor. A
 Função main deve imprimir o resultado da soma.
 */
+#include <stdio.h>
+
+int soma (int x, int y){
+    return x + y;
+}
+
+int main (){
+    int a, b, c;
+
+    printf("1st number:\n");
+    scanf("%d", &a);
+    printf("2nd number\n");
+    scanf("%d", &b);
+    
+    c = soma(a, b);
+
+    printf("Sum:\n%d", c);
+
+    return 0;
+}
