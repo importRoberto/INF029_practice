53cee5b63b900c43a9c5f9173129889bcf44734d
Roberto Barreto <robertoboficial@gmail.com>
2024-11-25 12:18:40 -0300

    Update and rename begin.txt to 1st_function.c

---
+++ 1st_function.c
@@ -0,0 +1,5 @@
+/*
+1) Crie um programa que tenha uma função soma e a função main. A função main deve ler dois
+valores, enviar para a função soma. A função soma deve realizar a soma e retornar o valor. A
+Função main deve imprimir o resultado da soma.
+*/
