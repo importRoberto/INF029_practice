da52e7219d702ee849dcaa8ff8e30f387b358264
Roberto Barreto <robertoboficial@gmail.com>
2024-11-29 12:08:35 -0300

    4th_function Complete.c

--- 4th_function.c
+++ 4th_function.c
@@ -1,5 +1,24 @@
-﻿/*
+/*
 4) Crie um programa que tenha uma função ler3Numeros. Essa função deve ler três números do
 usuário e retornar os três números. A função main deve imprimir esses três números. Utilize vetor
 para fazer o retorno
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void ler3Numeros(int num[3]){
+    for (int i = 0; i < 3; i++){
+        printf("Digite o %dº numero\n", i+1);
+        scanf("%d", &num[i]);
+    }
+}
+
+int main(){
+    int num[3];
+    ler3Numeros(num);
+
+    for (int j = 0; j < 3; j++){
+        printf("Numero %d == %d\n", j+1, num[j]);
+    }
+}
